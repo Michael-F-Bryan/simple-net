1e29aef5de7d616617db43b0e7cbc14196c2eede
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-19 17:05:39 +0000

    Deploy Michael-F-Bryan/simple-net to github.com/Michael-F-Bryan/simple-net.git:gh-pages

---
+++ implementors/core/ops/index/trait.Index.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["simple_net"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.tuple.html\">(</a><a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>, <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a><a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.tuple.html\">)</a>&gt; for <a class=\"struct\" href=\"simple_net/struct.Tensor.html\" title=\"struct simple_net::Tensor\">Tensor</a>",synthetic:false,types:["simple_net::tensor::Tensor"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
