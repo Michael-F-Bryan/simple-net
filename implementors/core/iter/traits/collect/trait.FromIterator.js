8983b75513a1c1876239d814b6e7d35d598d8e2c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-19 17:23:20 +0000

    Deploy Michael-F-Bryan/simple-net to github.com/Michael-F-Bryan/simple-net.git:gh-pages

---
+++ implementors/core/iter/traits/collect/trait.FromIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["simple_net"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.FromIterator.html\" title=\"trait core::iter::traits::collect::FromIterator\">FromIterator</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/boxed/struct.Box.html\" title=\"struct alloc::boxed::Box\">Box</a>&lt;dyn <a class=\"trait\" href=\"simple_net/trait.Layer.html\" title=\"trait simple_net::Layer\">Layer</a> + 'static&gt;&gt; for <a class=\"struct\" href=\"simple_net/struct.NeuralNet.html\" title=\"struct simple_net::NeuralNet\">NeuralNet</a>",synthetic:false,types:["simple_net::net::NeuralNet"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["simple_net"] = {"name":"","dirs":[],"files":["layers.rs","lib.rs","loss.rs","tensor.rs"]};
+sourcesIndex["simple_net"] = {"name":"","dirs":[],"files":["layers.rs","lib.rs","loss.rs","net.rs","tensor.rs"]};
 createSourceSidebar();
